f134a37148cdc6d19157344cce1fd2701affef88
Doud75 <adrienquimbre@gmail.com>
2023-06-28 22:12:08 +0200

    Feature adding getById and getAll

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -12,19 +12,27 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-
+/**
+ * controller des users
+ */
 class UserController extends AbstractController
 {
 
+    /**
+     * @param Request $request
+     * @param UserRepository $userRepository
+     * @param UserPasswordHasherInterface $passwordHasher
+     * @return JsonResponse
+     */
     #[Route('/user', name:'add_user', methods:['POST'])]
     public function create(
         Request $request,
         UserRepository $userRepository,
         UserPasswordHasherInterface $passwordHasher
         ):JsonResponse{
-        
+
         $data = json_decode($request->getContent(), true);
-        
+
         $user = new User();
         $user->setFirstName($data['first_name']);
         $user->setLastName($data['last_name']);
@@ -35,13 +43,13 @@ class UserController extends AbstractController
             $user,
             $data['password']
         );
-        
+
         $user->setPassword($hashedPassword);
 
         try{
             $userRepository->save($user,true);
             return $this->json([
-                'message' => 'Success add user', 
+                'message' => 'Success add user',
             ]);
         }catch(\Exception $e){
                 return $this->json([
@@ -49,25 +57,43 @@ class UserController extends AbstractController
             ]);
         }
     }
-    
 
-    #[Route('/user/{id}', name: 'get_user_Id',methods:['GET'])]
+
+    /**
+     * @param Uuid $id
+     * @param UserRepository $userRepository
+     * @return JsonResponse
+     */
+    #[Route('/user/one/{id}', name: 'get_user_Id',methods:['GET'])]
     public function getUserById(Uuid $id, UserRepository $userRepository): JsonResponse
     {
         $user = $userRepository->find($id);
+
+        if (!$user) {
+            return new JsonResponse(['error' => 'user not found'], 404);
+        }
+
         return new JsonResponse($user);
     }
 
-    #[Route('/user/all',name:'get_user_all',methods:['GET'])]
-    public function getAllUser(UserRepository $userRepository):JsonResponse
+    /**
+     * @param UserRepository $userRepository
+     * @return JsonResponse
+     */
+    #[Route('/user/all', name:'get_user_all', methods:['GET'])]
+    public function getAllUser(UserRepository $userRepository): JsonResponse
     {
-        
-        $user = $userRepository->findAll();
-        dd($user);
+        $users = $userRepository->findAll();
 
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'allUser' => 'src/Controller/UserController.php',
-        ]);
+        if (count($users) < 1) {
+            return new JsonResponse(['error' => 'user not found'], 404);
+        }
+
+        $serializedUser = [];
+        foreach ($users as $user) {
+            $serializedUser[] = $user->jsonSerialize();
+        }
+
+        return new JsonResponse($serializedUser);
     }
 }
