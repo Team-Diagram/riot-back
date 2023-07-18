c07b340e43f00b5cfec539aaf78de5c17caf310d
Romain Lhuillier <romainlhuillier94@gmail.com>
2023-07-18 10:01:23 +0200

    imprevement : jwt add lastname

---
+++ src/Event/JWTCreatedSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class JWTCreatedSubscriber implements EventSubscriberInterface
+{
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $data = $event->getData();
+
+        $user = $this->tokenStorage->getToken()->getUser();
+        $username = $user instanceof UserInterface ? $user->getLastName() : '';
+
+        // Ajoutez le nom de l'utilisateur au tableau $data
+        $data['username'] = $username;
+
+        $event->setData($data);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'lexik_jwt_authentication.on_jwt_created' => 'onJWTCreated',
+        ];
+    }
+}
