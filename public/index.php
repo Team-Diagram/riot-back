f1419c68521c2dc67debc5a90c816aaaf9b030b3
Romain Lhuillier <51294979+TisoOfficiel@users.noreply.github.com>
2023-07-19 21:28:36 +0200

    improvement : user controller update route,token data, correction header (#58)

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use App\Kernel;
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
+header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
 header('Allow: GET, POST, OPTIONS, PUT, DELETE');
 $method = $_SERVER['REQUEST_METHOD'];
